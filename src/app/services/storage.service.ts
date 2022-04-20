fab56f5b533739588cbfdc2aabaa3b12635b7d13
pruthvisoni1604 <pruthvisoni1604@gmail.com>
2022-04-20 04:11:49 -0400

    search func

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SQLite , SQLiteObject} from '@ionic-native/sqlite/ngx';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 import { IonItemSliding, Platform } from '@ionic/angular';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -8,38 +8,32 @@ import { AddRestaurantPage } from '../add-restaurant/add-restaurant.page';
 import { Restaurant } from './restaurant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
-
-  private storage : SQLiteObject;
+  private storage: SQLiteObject;
   restaurantList = new BehaviorSubject([]);
-  private isDbReady : BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private isDbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(
     private platform: Platform,
     private sqlite: SQLite,
     private httpClient: HttpClient,
     private sqlPorter: SQLitePorter
-
   ) {
+    this.platform.ready().then(() => {
+      this.sqlite
+        .create({
+          name: 'restaurant.db',
+          location: 'default',
+        })
 
-    this.platform.ready().then(()=>{
-      this.sqlite.create({
-        name:'restaurant.db',
-        location: 'default'
-       
-      })
-      
-      .then((db:SQLiteObject)=>{
+        .then((db: SQLiteObject) => {
           this.storage = db;
           this.getFakeDate();
-        }
-      )// this.init()
-    })
-   
-   }
-
+        }); // this.init()
+    });
+  }
 
   //  init(){
   //    return this.sqlite.create(
@@ -64,91 +58,143 @@ export class StorageService {
   //       });
   //       }
 
-
-
-  getFakeDate(){
-    this.httpClient.get('assets/data.sql', {responseType: 'text'})
-    .subscribe(data=>{
-      this.sqlPorter.importSqlToDb(this.storage, data)
-      .then(_=>{
-        this.getRestaurants();
-        this.isDbReady.next(true);
-      })
-      .catch(error => console.error(error));
-      
-    });
+  getFakeDate() {
+    this.httpClient
+      .get('assets/data.sql', { responseType: 'text' })
+      .subscribe((data) => {
+        this.sqlPorter
+          .importSqlToDb(this.storage, data)
+          .then((_) => {
+            this.getRestaurants();
+            this.isDbReady.next(true);
+          })
+          .catch((error) => console.error(error));
+      });
   }
 
-  dbState(){
+  dbState() {
     return this.isDbReady.asObservable();
   }
 
-  fetchRestaurant(): Observable<Restaurant[]>{
+  fetchRestaurant(): Observable<Restaurant[]> {
+    return this.restaurantList.asObservable();
+  }
+  fetchRestaurantByNameOrTag(str): Observable<Restaurant[]> {
     return this.restaurantList.asObservable();
   }
 
-  getRestaurants(){
-    return this.storage.executeSql('SELECT * FROM rtable' , []).then(res=>{
+  getRestaurants() {
+    return this.storage.executeSql('SELECT * FROM rtable', []).then((res) => {
       const items: Restaurant[] = [];
-      if(res.rows.length > 0){
-        for(let i = 0 ; i < res.rows.length; i++){
-          items.push(
-            {
-              id:res.rows.item(i).id,
-              rest_name:res.rows.item(i).rest_name,
-              rest_address:res.rows.item(i).rest_address,
-              rest_postal:res.rows.item(i).rest_postal,
-              rest_city:res.rows.item(i).rest_city,
-              rest_description:res.rows.item(i).rest_description,
-              rest_phone:res.rows.item(i).rest_phone,
-              rest_rating:res.rows.item(i).rest_rating
-            }
-          );
+      if (res.rows.length > 0) {
+        for (let i = 0; i < res.rows.length; i++) {
+          items.push({
+            id: res.rows.item(i).id,
+            rest_name: res.rows.item(i).rest_name,
+            rest_address: res.rows.item(i).rest_address,
+            rest_postal: res.rows.item(i).rest_postal,
+            rest_city: res.rows.item(i).rest_city,
+            rest_description: res.rows.item(i).rest_description,
+            rest_phone: res.rows.item(i).rest_phone,
+            rest_rating: res.rows.item(i).rest_rating,
+          });
         }
       }
       this.restaurantList.next(items);
     });
   }
-    //add
-    addRestaurant(rest_name, rest_address, rest_postal, rest_city, rest_description, rest_phone, rest_rating){
-        let data = [rest_name, rest_address, rest_postal, rest_city, rest_description, rest_phone, rest_rating];
-        return this.storage.executeSql('INSERT INTO rtable(rest_name, rest_address, rest_postal, rest_city, rest_description, rest_phone, rest_rating) VALUES(?,?,?,?,?,?,?)',data)
-        .then(res=>{
-          this.getRestaurants();
-        });
-    }
- //get
-
-  getRestaurantById(id): Promise<Restaurant>{
-      return this.storage.executeSql('SELECT * FROM rtable WHERE id = ?', [id])
-      .then(res=>({
-          id:res.rows.item(0).id,
-          rest_name:res.rows.item(0).rest_name,
-          rest_address:res.rows.item(0).rest_address,
-          rest_postal:res.rows.item(0).rest_postal,
-          rest_city:res.rows.item(0).rest_city,
-          rest_description:res.rows.item(0).rest_description,
-          rest_phone:res.rows.item(0).rest_phone,
-          rest_rating:res.rows.item(0).rest_rating
-
-        }));
-  }
- //update
-    updateRestaurant(id, restaurant:Restaurant){
-        let data = [restaurant.rest_name, restaurant.rest_address, restaurant.rest_postal, restaurant.rest_city, restaurant.rest_description, restaurant.rest_phone, restaurant.rest_rating]
-        return this.storage.executeSql(`UPDATE rtable SET rest_name = ? , rest_address = ? , rest_postal = ? , rest_city = ? , rest_description = ? , rest_phone = ? , rest_rating = ? WHERE id = ${id}` , data)
-        .then(
-          data =>{
-            this.getRestaurants();
-          }
-        )
-    }
-
-    //delete
-    deleteRestaurant(id){
-      return this.storage.executeSql('DELETE FROM rtable WHERE id = ?' , [id])
-      .then(res=>{
+  //add
+  addRestaurant(
+    rest_name,
+    rest_address,
+    rest_postal,
+    rest_city,
+    rest_description,
+    rest_phone,
+    rest_rating
+  ) {
+    let data = [
+      rest_name,
+      rest_address,
+      rest_postal,
+      rest_city,
+      rest_description,
+      rest_phone,
+      rest_rating,
+    ];
+    return this.storage
+      .executeSql(
+        'INSERT INTO rtable(rest_name, rest_address, rest_postal, rest_city, rest_description, rest_phone, rest_rating) VALUES(?,?,?,?,?,?,?)',
+        data
+      )
+      .then((res) => {
         this.getRestaurants();
-      })
-    }
+      });
+  }
+  //get
+
+  getRestaurantById(id): Promise<Restaurant> {
+    return this.storage
+      .executeSql('SELECT * FROM rtable WHERE id = ?', [id])
+      .then((res) => ({
+        id: res.rows.item(0).id,
+        rest_name: res.rows.item(0).rest_name,
+        rest_address: res.rows.item(0).rest_address,
+        rest_postal: res.rows.item(0).rest_postal,
+        rest_city: res.rows.item(0).rest_city,
+        rest_description: res.rows.item(0).rest_description,
+        rest_phone: res.rows.item(0).rest_phone,
+        rest_rating: res.rows.item(0).rest_rating,
+      }));
+  }
+  //search by name or tag
+  getRestaurantsByNameOrTag(str): Promise<void | Restaurant> {
+    return this.storage.executeSql('SELECT * FROM rtable WHERE rest_name = ?', [str]).then((res) => {
+      const items: Restaurant[] = [];
+      if (res.rows.length > 0) {
+        for (let i = 0; i < res.rows.length; i++) {
+          items.push({
+            id: res.rows.item(i).id,
+            rest_name: res.rows.item(i).rest_name,
+            rest_address: res.rows.item(i).rest_address,
+            rest_postal: res.rows.item(i).rest_postal,
+            rest_city: res.rows.item(i).rest_city,
+            rest_description: res.rows.item(i).rest_description,
+            rest_phone: res.rows.item(i).rest_phone,
+            rest_rating: res.rows.item(i).rest_rating,
+          });
+        }
+      }
+      this.restaurantList.next(items);
+    });
+  }
+  //update
+  updateRestaurant(id, restaurant: Restaurant) {
+    let data = [
+      restaurant.rest_name,
+      restaurant.rest_address,
+      restaurant.rest_postal,
+      restaurant.rest_city,
+      restaurant.rest_description,
+      restaurant.rest_phone,
+      restaurant.rest_rating,
+    ];
+    return this.storage
+      .executeSql(
+        `UPDATE rtable SET rest_name = ? , rest_address = ? , rest_postal = ? , rest_city = ? , rest_description = ? , rest_phone = ? , rest_rating = ? WHERE id = ${id}`,
+        data
+      )
+      .then((data) => {
+        this.getRestaurants();
+      });
+  }
+
+  //delete
+  deleteRestaurant(id) {
+    return this.storage
+      .executeSql('DELETE FROM rtable WHERE id = ?', [id])
+      .then((res) => {
+        this.getRestaurants();
+      });
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  str: string;
+  data: any[] = [];
+  constructor(private db: StorageService,private toast: ToastController) {
+  }
+  searchItem(){
+    this.db.getRestaurantsByNameOrTag(this.str).then(res => {
+      if(res instanceof Object){
+        this.presentToast('d');
+        this.presentToast(res.rest_name);
 
-  constructor() {}
+      }else{
+        this.presentToast(res[0].toString());
+        this.presentToast("res[0].rest_name");
 
+      }
+    }, err=>{
+      this.presentToast('e');
+    });
+  }
+  async presentToast(str) {
+    let toast = await this.toast.create({
+      message: str,
+      duration: 3000,
+    });
+    toast.present();
+   }
 }
